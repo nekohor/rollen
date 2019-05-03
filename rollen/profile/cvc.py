524ccc110f12de7f337b4decb1a36822cd126309
nekohor <nekohor@foxmail.com>
2019-05-03 20:43:33 +0800

    re code get_crn_array get_shft_array

--- rollen/profile/cvc.py
+++ rollen/profile/cvc.py
@@ -1,3 +1,4 @@
+import numpy as np
 from .std import Stand
 from .roll import Roll
 
@@ -12,11 +13,18 @@ class ContinuouslyVariableCrown():
 
         self.abs_max_strock = 150
 
+        self.edge_drop = 40
+
     def get_max_strock(self):
         return self.abs_max_strock
 
     def get_min_strock(self):
         return - self.abs_max_strock
+
+    def get_shft_array(self):
+        return np.arange(
+            self.get_min_strock,
+            self.get_max_strock + 1)
 
     def get_default_max_crn(self, std):
         if std in self.stand.upstreams:
@@ -44,6 +52,9 @@ class ContinuouslyVariableCrown():
 
     def get_default_half_wid(self):
         return self.get_default_wid() / 2
+
+    def get_half_wid(self, wid):
+        return (wid - 2 * self.edge_drop) / 2
 
     def get_prof_coefs(self,
                        max_crn=None, min_crn=None,
@@ -79,4 +90,33 @@ class ContinuouslyVariableCrown():
             b = self.get_default_half_wid()
             a1 = - 2 * a2 * L - 3 * a3 * L * L - a3 * b * b
 
-        return (a1, a2, a3)
+        return [0, a1, a2, a3]
+
+    def get_crn(self, wid, pos_shft, coefs):
+
+        Lwr = self.roll.get_len()
+        L = self.get_half_len()
+        b = self.get_half_wid(wid)
+
+        Ps = L - pos_shft
+        Psb = L + pos_shft
+
+        p = np.poly1d(coefs[::-1])
+        D = p(Lwr)
+
+        Hc = D - p(Lwr - Psb) - p(Ps)
+        Hd = D - p(Lwr - (Psb + b)) - p(Ps + b)
+        Ho = D - p(Lwr - (Psb - b)) - p(Ps - b)
+
+        # crown negative value and with um
+        # Note that the crown is roll crown, not the strip crown
+        crn = -1 * (Hc - 0.5 * (Hd + Ho)) * 1000
+
+        return crn
+
+    def get_crn_array(self, wid, coefs):
+        crn_array = np.array([])
+        for shft in self.get_shft_array():
+            crn_array = np.append(crn_array, self.get_crn(shft, wid, coefs))
+
+    def get_curve(self, wid, max_crn=None, min_crn=None):
